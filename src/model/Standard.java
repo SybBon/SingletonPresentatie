58c17b6921e8f759150423a2cbb90c9ee3a5dddb
SybBon <sybrenb@gmail.com>
2025-11-03 13:43:33 +0100

    Standaard methode toegevoegd

--- src/model/Standard.java
+++ src/model/Standard.java
@@ -5,5 +5,17 @@ package model;
  * TODO
  */
 public class Standard {
+    private int kledingStukken;
+    private String owner;
+
+    public Standard(String owner) {
+        this.owner = owner;
+        kledingStukken = 0;
+    }
+
+    public void addLaundry(String item) {
+        kledingStukken++;
+        System.out.println("Toegevoegd " + item + " aan " + owner + "'s wasmand. Totaal items: " + kledingStukken);
+    }
 
 }
